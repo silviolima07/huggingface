31d881fc0d6ba8a8973f5b1e82f787973090ed7f
Silvio Lima <silviolima07@gmail.com>
2025-01-20 09:39:07 -0300

    teste

--- utils.py
+++ utils.py
@@ -1,34 +1,13 @@
-import socket
-import requests
-from ip2geotools.databases.noncommercial import DbIpCity
-from geopy.geocoders import Nominatim
-import streamlit as st
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import torch
 
-# calling the Nominatim tool and create Nominatim class
-loc = Nominatim(user_agent="Geopy Library")
+@st.cache_resource
+def load_model():
+    model_path = "TucanoBR/ViTucano-1b5-v1"
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
-
-def save_uploaded_file(uploaded_file, destination_path):
-    with open(destination_path, "wb") as f:
-        f.write(uploaded_file.read())
-    return destination_path
-
-def printDetails(ip):
-    res = DbIpCity.get(ip, api_key="free")
-    st.write(f"IP Address: {res.ip_address}")
-    #st.write(f"Location: {res.city}, {res.region}, {res.country}")
-    #st.write(f"Coordinates: (Lat: {res.latitude}, Lng: {res.longitude})")
-    
-    # entering the location name
-    getLoc = loc.geocode({res.city})
-
-    # printing address
-    #st.write(getLoc.address)
-
-    # printing latitude and longitude
-    #st.write("Latitude = ", getLoc.latitude, "\n")
-    #st.write("Longitude = ", getLoc.longitude)
-    
-    return res, getLoc
+    model = AutoModelForCausalLM.from_pretrained(model_path) #, trust_remote_code=True)
+    model.to(device)
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    return tokenizer, model
     
